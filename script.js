8b7089520076c5f50fb33fbe4dbbfd56e424d436
Suvam Chauhan <chauhansuvam@gmail.com>
2024-03-04 10:36:59 -0500

    background commit

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 $(document).ready(function () {
   $("#toggle-switch").click(function () {
     $(this).toggleClass("on");
+    var backgroundColor = $(this).hasClass("on") ? "rgb(15, 104, 20)" : "rgb(192, 51, 16)"; // Determine the background color based on the switch state
+    $("body").css("background-color", backgroundColor); // Update the background color
     if ($(this).hasClass("on")) {
-      $("body").animate({ backgroundColor: "lightgreen" }, 500); // To change background color when ON
       $('#switch-message').text('Switch Is Turned ON'); // To Update the message when the switch is turned on
       setTimeout(function(){
         $('#switch-message').text(''); // To Clear the message after a few seconds when turned on
       }, 1000); 
     } else {
-      $("body").animate({ backgroundColor: "white" }, 500); //  To Smoothly revert background color when OFF
       $('#switch-message').text('Switch Is Turned OFF'); // To Update the message when the switch is turned off
       setTimeout(function(){
-        $('#switch-message').text(''); // To Clear the message after a few seconds mwhen turned off
+        $('#switch-message').text(''); // To Clear the message after a few seconds when turned off
       }, 1000); 
     }
-    $(this).animate({ left: $(this).hasClass("on") ? 0 : 60 }, 300); // for sliding animation for the toggle switch
+    // Slide animation for the toggle switch
+    $(this).animate({ left: $(this).hasClass("on") ? 0 : 60 }, 300); 
   });
 });
