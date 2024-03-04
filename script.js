5eba285f3f8648492a179581c05e924c1f513ed1
Suvam Chauhan <chauhansuvam@gmail.com>
2024-03-04 11:38:16 -0500

    final changes commit

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 $(document).ready(function () {
   $("#toggle-switch").click(function () {
     $(this).toggleClass("on");
-    var backgroundColor = $(this).hasClass("on") ? "rgb(15, 104, 20)" : "rgb(192, 51, 16)"; // Determine the background color based on the switch state
-    $("body").css("background-color", backgroundColor); // Update the background color
-    if ($(this).hasClass("on")) {
-      $('#switch-message').text('Switch Is Turned ON'); // To Update the message when the switch is turned on
+
+    // for the background color based on the switch state
+    var backgroundColor = $(this).hasClass("on") ? "rgb(15, 104, 20)" : "rgb(192, 51, 16)"; 
+    
+    // To Update the background color
+    $("body").css("background-color", backgroundColor); 
+    if 
+    
+    ($(this).hasClass("on")) {
+
+      // To Update the message when the switch is turned on
+      $('#switch-message').text('Switch Is Turned ON'); 
       setTimeout(function(){
-        $('#switch-message').text(''); // To Clear the message after a few seconds when turned on
-      }, 1000); 
+
+        // To set the message  only for few seconds when turned on
+        $('#switch-message').text(''); 
+      }, 3000);
+
     } else {
-      $('#switch-message').text('Switch Is Turned OFF'); // To Update the message when the switch is turned off
+
+      // To Update the message when the switch is turned off
+      $('#switch-message').text('Switch Is Turned OFF'); 
       setTimeout(function(){
-        $('#switch-message').text(''); // To Clear the message after a few seconds when turned off
-      }, 1000); 
+
+      // To set the message  only for few seconds when turned off
+        $('#switch-message').text(''); 
+      }, 3000); 
     }
     // To Slide animation for the toggle switch
     $(this).animate({ left: $(this).hasClass("on") ? 0 : 60 }, 300); 
